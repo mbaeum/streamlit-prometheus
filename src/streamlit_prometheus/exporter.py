36b916cac2d35d50466e39c4a48059c54c059a58
Moritz Baeumel <moritzbaeumnel@gmail.com>
2026-02-08 16:58:49 +0100

    extend exporter

--- src/streamlit_prometheus/exporter.py
+++ src/streamlit_prometheus/exporter.py
@@ -6,23 +6,35 @@ from prometheus_client import start_http_server
 
 logger = logging.getLogger("streamlit-observe")
 
+_DEFAULT_HOST = "127.0.0.1"
 
-def _port_is_open(port: int, host: str = "127.0.0.1") -> bool:
+
+def _port_is_open(port: int, host: str = _DEFAULT_HOST) -> bool:
     with socket(AF_INET, SOCK_STREAM) as sock:
         sock.settimeout(0.5)
         return sock.connect_ex((host, port)) == 0
 
 
-def start_exporter(port: int):
+def _start_exporter(port: int, host: str = _DEFAULT_HOST):
     logger.info(f"Starting Prometheus exporter on :{port}")
-    start_http_server(port)
+    start_http_server(port=port, addr=host)
 
 
-def start_exporter_thread(port: int):
+def _start_exporter_thread(port: int, host: str = _DEFAULT_HOST):
     thread = Thread(
-        target=start_exporter,
-        args=(port,),
+        target=_start_exporter,
+        args=(
+            port,
+            host,
+        ),
         daemon=True,
         name="streamlit-observe-exporter",
     )
     thread.start()
+
+
+def _start_once(port: int, host: str = _DEFAULT_HOST):
+    if not _port_is_open(port, host):
+        logger.info(f"Exporter already running on port {port}")
+    else:
+        _start_exporter_thread(port)
--- tests/test_exporter.py
+++ tests/test_exporter.py
@@ -3,8 +3,9 @@ from unittest.mock import MagicMock, patch
 
 from streamlit_prometheus.exporter import (
     _port_is_open,
-    start_exporter,
-    start_exporter_thread,
+    _start_exporter,
+    _start_exporter_thread,
+    _DEFAULT_HOST
 )
 
 
@@ -18,7 +19,7 @@ def test__port_is_open(mock_socket_cls):
 
     assert result is True
     mock_sock.settimeout.assert_called_once_with(0.5)
-    mock_sock.connect_ex.assert_called_once_with(("127.0.0.1", 8080))
+    mock_sock.connect_ex.assert_called_once_with((_DEFAULT_HOST, 8080))
 
 
 @patch("streamlit_prometheus.exporter.socket")
@@ -47,9 +48,9 @@ def test_custom_host(mock_socket_cls):
 def test_start_exporter_starts_server_and_logs(mock_start_http_server):
     port = 8000
 
-    start_exporter(port)
+    _start_exporter(port)
 
-    mock_start_http_server.assert_called_once_with(port)
+    mock_start_http_server.assert_called_once_with(port=port, addr=_DEFAULT_HOST)
 
 
 @patch("streamlit_prometheus.exporter.Thread")
@@ -57,7 +58,7 @@ def test_start_exporter_thread_creates_and_starts_thread(mock_thread_cls):
     mock_thread = MagicMock()
     mock_thread_cls.return_value = mock_thread
 
-    start_exporter_thread(9100)
+    _start_exporter_thread(9100)
 
     mock_thread_cls.assert_called_once()
     mock_thread.start.assert_called_once()
